2526428db5af0d5a58cc0b467d943506649f4f25
Adam Faur <adam.faur@gmail.com>
2016-05-17 22:57:09 -0500

    Initial Commit.

---
+++ readme.md
@@ -0,0 +1,19 @@
+## ASCII Search
+
+Search for ASCII characters by name or by number.
+
+## Installing
+
+Add `tinytacoteam/ascii-calculator` inside of `plugins` block of your  `~/.zazurc.js` file.
+
+~~~ javascript
+module.exports = {
+  // ...
+  'plugins': [
+    'tinytacoteam/ascii-calculator',
+    // ...
+  ],
+  // ...
+}
+~~~
+
---
+++ zazu.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: 'ascii-search',
+  version: '0.0.1',
+  author: 'afaur',
+  description: 'Search for ASCII characters by name or by number.',
+  icon: 'icon.png',
+  homepage: 'https://github.com/tinytacoteam/ascii-search',
+  git: 'git@github.com:tinytacoteam/ascii-search.git',
+  blocks: {
+    input: [
+      {
+        id: 1,
+        type: 'PrefixScript',
+        prefix: 'asc',
+        space: 'Required',
+        script: 'node ascii_search.js {query}',
+        connections: [2]
+      },
+    ],
+    output: [
+      {
+        id: 2,
+        type: 'CopyToClipboard',
+        text: '{value}',
+      },
+    ],
+  },
+}
